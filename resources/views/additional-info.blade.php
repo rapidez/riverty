e55b81ef9901cf755949c72878158980c0a09ed7
Jade Geels <a@jgw.tf>
2026-06-10 14:52:04 +0200

    Fix broken frontend from v3 upgrade

--- resources/views/additional-info.blade.php
+++ resources/views/additional-info.blade.php
@@ -1,5 +1,5 @@
 <template v-else-if="method.code.includes('riverty_') || method.code.includes('afterpay_')">
-    <x-rapidez::radio
+    <x-rapidez::input.radio
         name="payment_method"
         v-model="variables.code"
         v-bind:value="method.code"
@@ -8,7 +8,7 @@
         required
         >
         @{{ method.title }}
-    </x-rapidez::radio>
+    </x-rapidez::input.radio>
     <div class="mt-2" v-if="variables.code === method.code">
         <span class="my-1">@lang('In order to use Riverty to pay, we need the following information.')</span>
         <x-rapidez::label :label="__('Date of Birth')" class="my-1">
@@ -21,20 +21,20 @@
         </x-rapidez::label>
         <x-rapidez::label :label="__('Gender')" class="my-1">
             <div class="flex">
-                <x-rapidez::radio
+                <x-rapidez::input.radio
                     name="gender"
                     value="1"
                     v-model="custom.gender"
                 >
                     <div>@lang('Male')</div>
-                </x-rapidez::radio>
-                <x-rapidez::radio
+                </x-rapidez::input.radio>
+                <x-rapidez::input.radio
                     name="gender"
                     value="2"
                     v-model="custom.gender"
                 >
                     <div>@lang('Female')</div>
-                </x-rapidez::radio>
+                </x-rapidez::input.radio>
             </div>
         </x-rapidez::label>
     </div>
